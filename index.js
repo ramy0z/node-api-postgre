a553aae9b5ee2b7cf56dcdb46ccdaa4e0664b40e
Ramy Ezz <ramy0z@yahoo.com>
2020-04-16 14:00:30 +0200

    fc

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const db = require('./queries')
-const port = 3000
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json())
 app.use(
@@ -21,6 +21,6 @@ app.post('/users', db.createUser)
 app.put('/users/:id', db.updateUser)
 app.delete('/users/:id', db.deleteUser)
 
-const PORT = process.env.PORT || 3000;
-createApp()
-.then(app => app.listen(PORT, () => {console.log(`Our app is running on port ${ PORT }`)}))
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}.`)
+})
