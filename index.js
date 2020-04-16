b7c312d36eb77109de6a9e597657a6d4318906b8
Ramy Ezz <ramy0z@yahoo.com>
2020-04-16 14:30:44 +0200

    fc

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(
 )
 
 app.get('/', (request, response) => {
-  response.json({ info: 'Node.js, Express, and Postgres API   2222222222222222222' })
+  response.json({ info: 'Node.js, Express, and Postgres API' })
 })
 
 app.get('/users', db.getMyUsers)
